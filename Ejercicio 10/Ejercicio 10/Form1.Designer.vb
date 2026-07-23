87ab681a372d2353996a4753ae6220472d5e5552
MaryQ <andresrusby@gmail.com>
2026-07-22 20:37:48 -0500

    nuevo archivo

---
+++ Ejercicio 10/Ejercicio 10/Form1.Designer.vb
@@ -0,0 +1,342 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        GroupBox1 = New GroupBox()
+        RadioButton3 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        RadioButton1 = New RadioButton()
+        GroupBox2 = New GroupBox()
+        RadioButton5 = New RadioButton()
+        RadioButton4 = New RadioButton()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        CheckBox1 = New CheckBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox5 = New TextBox()
+        TextBox6 = New TextBox()
+        TextBox7 = New TextBox()
+        GroupBox1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(50, 27)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(103, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Ingrese Nombres :"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(53, 71)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(103, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Ingrese Apellidos :"
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(RadioButton3)
+        GroupBox1.Controls.Add(RadioButton2)
+        GroupBox1.Controls.Add(RadioButton1)
+        GroupBox1.Location = New Point(33, 148)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(241, 135)
+        GroupBox1.TabIndex = 2
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Especialidad :"
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.AutoSize = True
+        RadioButton3.Location = New Point(24, 100)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(103, 19)
+        RadioButton3.TabIndex = 2
+        RadioButton3.TabStop = True
+        RadioButton3.Text = "Protesis Dental"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(23, 59)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(93, 19)
+        RadioButton2.TabIndex = 1
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "Contabilidad"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(24, 21)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(175, 19)
+        RadioButton1.TabIndex = 0
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "Diseño y Programación Web"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Controls.Add(RadioButton5)
+        GroupBox2.Controls.Add(RadioButton4)
+        GroupBox2.Location = New Point(297, 106)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(200, 100)
+        GroupBox2.TabIndex = 3
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Condicion:"
+        ' 
+        ' RadioButton5
+        ' 
+        RadioButton5.AutoSize = True
+        RadioButton5.Location = New Point(30, 67)
+        RadioButton5.Name = "RadioButton5"
+        RadioButton5.Size = New Size(79, 19)
+        RadioButton5.TabIndex = 1
+        RadioButton5.TabStop = True
+        RadioButton5.Text = "AFP (12%)"
+        RadioButton5.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton4
+        ' 
+        RadioButton4.AutoSize = True
+        RadioButton4.Location = New Point(31, 23)
+        RadioButton4.Name = "RadioButton4"
+        RadioButton4.Size = New Size(83, 19)
+        RadioButton4.TabIndex = 0
+        RadioButton4.TabStop = True
+        RadioButton4.Text = "ONP (13%)"
+        RadioButton4.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(62, 110)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(80, 15)
+        Label3.TabIndex = 4
+        Label3.Text = "Ingrese Edad :"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(53, 312)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(49, 15)
+        Label4.TabIndex = 5
+        Label4.Text = "Sueldo :"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(50, 355)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(79, 15)
+        Label5.TabIndex = 6
+        Label5.Text = "Bonificación :"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(53, 392)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(49, 15)
+        Label6.TabIndex = 7
+        Label6.Text = "Aporte :"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(53, 431)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(38, 15)
+        Label7.TabIndex = 8
+        Label7.Text = "Total :"
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(305, 235)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(98, 19)
+        CheckBox1.TabIndex = 9
+        CheckBox1.Text = "Bonificación :"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(328, 299)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 10
+        Button1.Text = "Calcular"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(328, 351)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 11
+        Button2.Text = "Nuevo"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(328, 397)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(75, 23)
+        Button3.TabIndex = 12
+        Button3.Text = "Salida"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(174, 24)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 13
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(174, 63)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 14
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(174, 102)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(100, 23)
+        TextBox3.TabIndex = 15
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(150, 304)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(100, 23)
+        TextBox4.TabIndex = 16
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(150, 347)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(100, 23)
+        TextBox5.TabIndex = 17
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Location = New Point(149, 389)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(100, 23)
+        TextBox6.TabIndex = 18
+        ' 
+        ' TextBox7
+        ' 
+        TextBox7.Location = New Point(149, 431)
+        TextBox7.Name = "TextBox7"
+        TextBox7.Size = New Size(100, 23)
+        TextBox7.TabIndex = 19
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 487)
+        Controls.Add(TextBox7)
+        Controls.Add(TextBox6)
+        Controls.Add(TextBox5)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(CheckBox1)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton5 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+
+End Class
---
+++ Ejercicio 10/Ejercicio 10/Form1.vb
@@ -0,0 +1,59 @@
+﻿Public Class Form1
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+        TextBox4.Clear()
+        TextBox5.Clear()
+        TextBox6.Clear()
+        TextBox7.Clear()
+        RadioButton1.Checked = False
+        RadioButton2.Checked = False
+        RadioButton3.Checked = False
+        RadioButton4.Checked = False
+        RadioButton5.Checked = False
+        CheckBox1.Checked = False
+        TextBox1.Focus()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+        Dim bonificación, sueldo, aporte, total As Integer
+
+
+        bonificación = CheckBox1.Checked
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = True Then
+            TextBox5.Text = 100
+        End If
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        If RadioButton1.Checked = True Then
+            TextBox4.Text = 2500
+        End If
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        If RadioButton2.Checked = True Then
+            TextBox4.Text = 2400
+        End If
+    End Sub
+
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+        If RadioButton3.Checked = True Then
+            TextBox4.Text = 2000
+        End If
+    End Sub
+
+    Private Sub RadioButton4_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton4.CheckedChanged
+        Dim onp, afp As Integer
+        onp = Val(TextBox6.Text)
+        If RadioButton1.Checked = True Then
+    End Sub
+End Class
